97bc3a697b23f7121d1f3fad66cde95a3a93bbba
Adnan Saleem <adnan13893@gmail.com>
2024-01-17 19:50:56 +0500

    daisy ui setup and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: [
+      {
+        darkTheme: {
+          primary: "#a700ff",
+          secondary: "#00c100",
+          accent: "#0057ff",
+          neutral: "#172537",
+          "base-100": "#182532",
+          info: "#5fcfff",
+          success: "#00e287",
+          warning: "#ff8200",
+          error: "#b92843",
+          body: {
+            "background-color": "#182532",
+          },
+        },
+        lightTheme: {
+          primary: "#0000ff",
+          secondary: "#00ebff",
+          accent: "#003bff",
+          neutral: "#121815",
+          "base-100": "#fff7fd",
+          info: "#00ffff",
+          success: "#85f94e",
+          warning: "#ff9b00",
+          error: "#e12250",
+          body: {
+            "background-color": "#fff7fd",
+          },
+        },
       },
-    },
+    ],
+    prefix: "tw-daisy-",
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
