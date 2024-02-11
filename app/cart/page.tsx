9bad2542727a33bbca51d1df2bf71865c51aa4f9
Adnan Saleem <adnan13893@gmail.com>
2024-02-11 23:11:25 +0500

    cart page done

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,7 @@
 import NoResultText from "@/components/atoms/Typography/NoResultText";
 import CartItem from "@/components/molecules/Cart/CartItem";
 import { CartItemWithProduct, getCart } from "@/utils/db/cart";
+import { formatPrice } from "@/utils/helper-methods";
 
 export default async function CartPage() {
   const cart = await getCart();
@@ -18,6 +19,18 @@ export default async function CartPage() {
               />
             );
           })}
+
+          <div className="flex w-full flex-col items-center justify-between">
+            <div className="my-8 h-[2px] w-full bg-black"></div>
+            <div className="flex w-full items-center justify-between">
+              <h4 className="text-2xl font-semibold uppercase text-gray-600">
+                Sub Total :
+              </h4>
+              <span className="text-4xl font-bold">
+                {formatPrice(cart.subTotal)}
+              </span>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="mx-auto mt-8 w-full text-center">
