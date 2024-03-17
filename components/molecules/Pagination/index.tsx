dd1c8a0cffa7d8834707924531fdf4cd0b54b763
Adnan Saleem <adnan13893@gmail.com>
2024-03-18 01:02:05 +0500

    pagination button size updated

--- components/molecules/Pagination/index.tsx
+++ components/molecules/Pagination/index.tsx
@@ -5,10 +5,10 @@ type Props = { totalCount: number };
 export default function Pagination({ totalCount }: Props) {
   return (
     <div className="join gap-x-1">
-      <button className="btn join-item btn-sm">1</button>
-      <button className="btn join-item btn-active btn-sm">2</button>
-      <button className="btn join-item btn-sm">3</button>
-      <button className="btn join-item btn-sm">4</button>
+      <button className="btn join-item">1</button>
+      <button className="btn join-item btn-active">2</button>
+      <button className="btn join-item">3</button>
+      <button className="btn join-item">4</button>
     </div>
   );
 }
