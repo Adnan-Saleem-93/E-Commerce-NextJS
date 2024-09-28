427ed53108a422650558cae3f776b8c29d652f84
Adnan Saleem <adnan13893@gmail.com>
2024-09-29 01:13:38 +0500

    updated product card design

--- components/molecules/ProductCard/index.tsx
+++ components/molecules/ProductCard/index.tsx
@@ -13,30 +13,27 @@ const ProductCard = ({ product }: Props) => {
     Date.now() - new Date(createdAt).getTime() < 1000 * 60 * 60 * 24 * 7;
 
   return (
-    <Link href={`/product/${id}`} className="col-span-1 min-h-[400px]">
-      <div className="card h-full w-full bg-base-100">
-        <figure className="h-1/2">
-          <Image
-            src={imageUrl}
-            alt={name.toLowerCase().replaceAll(" ", "-")}
-            width={384}
-            height={226.75}
-            className="h-full max-h-60 min-h-60 w-full object-cover"
-          />
-        </figure>
-        <div className="card-body h-1/2 justify-between">
-          <h2 className="card-title">{name}</h2>
-          {isNew && (
-            <p className="badge badge-warning max-h-6 min-h-6 text-white">
-              NEW!
-            </p>
-          )}
-          <p className="line-clamp-3 flex-grow-0">{description}</p>
+    <Link
+      href={`/product/${id}`}
+      className="col-span-1 flex min-h-[400px] flex-col gap-y-4"
+    >
+      <Image
+        src={imageUrl}
+        alt={name.toLowerCase().replaceAll(" ", "-")}
+        width={384}
+        height={226.75}
+        className="h-full max-h-60 min-h-60 w-full rounded-xl object-cover hover:shadow-xl"
+      />
+      <div className="h-1/2 justify-between p-2">
+        <h2 className="card-title">{name}</h2>
+        {isNew && (
+          <p className="badge badge-warning max-h-6 min-h-6 text-white">NEW!</p>
+        )}
+        <p className="line-clamp-3 flex-grow-0">{description}</p>
 
-          <div className="card-actions items-center justify-between">
-            <RatingOverview rating={rating} />
-            <PriceTag price={price} className="text-xl font-bold" />
-          </div>
+        <div className="card-actions items-center justify-between">
+          <RatingOverview rating={rating} />
+          <PriceTag price={price} className="text-xl font-bold" />
         </div>
       </div>
     </Link>
